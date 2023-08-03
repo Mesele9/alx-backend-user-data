21ce4fd466377db3873e5a15468b7d1bbd6bedf2
Mesele Assefa <getmesy@gmail.com>
2023-08-03 23:06:32 +0000

    annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 """ filtered_logger.py """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str
+                 ) -> str:
     """ a function that returns the log message obfuscated."""
     pattern = fr'\b({"|".join(fields)})=([^{separator}]+)'
     return re.sub(pattern, fr'\1={redaction}', message)
