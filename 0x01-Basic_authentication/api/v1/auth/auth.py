8f756e3898ae653fe31b4123d2c77c47cc42fe96
Mesele Assefa <getmesy@gmail.com>
2023-08-09 20:30:06 +0300

    done task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,17 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ a method that returns false """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path.endswith('/') and excluded_path.endswith('/'):
+                if path == excluded_path:
+                    return False
+            elif path + '/' == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ a method that returns None"""
