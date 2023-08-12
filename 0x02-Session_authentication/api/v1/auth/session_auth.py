9be447e2e54315250ec197aa643db334d49aa6cf
Mesele Assefa <getmesy@gmail.com>
2023-08-12 22:48:37 +0300

    done task6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ session_auth """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -24,3 +25,15 @@ class SessionAuth(Auth):
             return None
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
+
+    def current_user(self, request=None):
+        """ an instance method that returns a User instance based on a
+        cookie value """
+        if request is None:
+            return None
+
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
