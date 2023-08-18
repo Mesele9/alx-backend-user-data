bd742e258aac7bb5e2edaf4ef605c0b9fd6f261b
Mesele Assefa <getmesy@gmail.com>
2023-08-18 14:20:24 +0000

    pycodestyle

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,4 +51,4 @@ class DB:
         user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound
-        return user 
+        return user
