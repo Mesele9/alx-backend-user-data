342d4ad70985ff126e5a1eac3706ccfb2d575600
Mesele Assefa <getmesy@gmail.com>
2023-08-18 15:37:33 +0000

    done task5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ auth.py """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +12,22 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """  a method that checks and register user """
+        user = self._db._session.query(User).filter_by(email=email).first()
+        if user:
+            raise ValueError(f"User {email} already exists")
+        hashed_password = _hash_password(password)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._db._session.add(new_user)
+        self._db._session.commit()
+        return new_user
