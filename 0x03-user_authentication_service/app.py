f9ceefa8870a16cb7ffd9c9a5442fa5d8abbb614
Mesele Assefa <getmesy@gmail.com>
2023-08-19 02:46:52 +0000

    done task15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,5 +55,18 @@ def logout():
         abort(403)
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    """ a function that respond to /profile"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        response = jsonify({"email": user.email})
+        return response, 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
