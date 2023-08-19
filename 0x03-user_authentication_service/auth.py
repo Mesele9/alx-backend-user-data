ec796632f37941444e0f988e10e86810501bb4a0
Mesele Assefa <getmesy@gmail.com>
2023-08-19 02:06:40 +0000

    doen task13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,3 +66,8 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int):
+        user = self._db.find_user_by(id=user_id)
+        user.session_id = None
+        self._db._session.commit()
