63fbe09799b23c53a242c658e0c264c35e91e431
Mesele Assefa <getmesy@gmail.com>
2023-08-20 16:25:44 +0000

    docuemntation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,6 +10,7 @@ AUTH = Auth()
 
 @app.route("/")
 def index():
+    """ welcome page """
     return jsonify({"message": "Bienvenue"})
 
 
@@ -45,6 +46,7 @@ def login():
 
 @app.route("/sessions", methods=["DELETE"])
 def logout():
+    """ a fucntion that implemets logout """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
 
